ab28a4f2a0866159da16549161edf28106160218
Rama Nugraha <rama77539@gmail.com>
2022-06-22 02:15:02 +0700

    first commit

---
+++ app/src/main/java/id/ac/atmaluhur/uasdpm2011500030/DataCampuss.kt
@@ -0,0 +1,11 @@
+package id.ac.atmaluhur.uasdpm2011500030
+
+class DataCampuss (
+    var nidn: String,
+    var nmDosen: String,
+    var jabatan: String,
+    var golonganpangkat: String,
+    var pendidikan: String,
+    var bidangkeahlian: String,
+    var programstudi: String
+)
